4a9fba6456ac7945f548f7d73c5e9aef2d58a09b
csgear <csgear@live.com>
2017-07-06 11:28:48 +0800

    add solutions

---
+++ python/findDerangement.py
@@ -0,0 +1,13 @@
+
+### leetcode.com  634. Find the Derangement of An Array
+
+class Solution(object):
+    def findDerangement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0, 1]
+        for x in range(2, n + 1):
+            dp.append(x * (dp[- 1] + dp[-2]) % (10**9 + 7))
+        return dp[n - 1]
