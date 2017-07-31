c2be1988d2a5de083356e947e865598bf60ed99f
csgear <csgear@live.com>
2017-07-31 14:07:38 +0800

    format code

--- c++/detectCapitalUse.cpp
+++ c++/detectCapitalUse.cpp
@@ -4,14 +4,15 @@
 #include <string>
 #include <algorithm>
 
-using namespace std ;
+using namespace std;
 
-class Solution {
-public:
-    bool detectCapitalUse(string word) {
-        
-        int cnt = count_if(word.begin(), word.end(), [](char c) { return c <= 'Z' ; }) ;
-        
-        return (cnt == 0 || (cnt == word.size() || (cnt == 1 && word[0] <= 'Z'))) ;
+class Solution
+{
+  public:
+    bool detectCapitalUse(string word)
+    {
+
+        int cnt = count_if(word.begin(), word.end(), [](char c) { return c <= 'Z'; });
+        return (cnt == 0 || (cnt == word.size() || (cnt == 1 && word[0] <= 'Z')));
     }
 };
