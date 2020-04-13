4ee9b65383457a118655371632b4d12f2f659216
Xiaojun Yang <csgear@live.com>
2020-04-12 21:16:29 -0400

    foundations

---
+++ jdk11ocp/features/src/test/java/com/vkeonline/enthuware/PrimitiveDataTypeTest.java
@@ -0,0 +1,13 @@
+package com.vkeonline.enthuware;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimitiveDataTypeTest {
+    static PrimitiveDataType pdt = new PrimitiveDataType() ;
+    @Test
+    void myMethod() {
+        pdt.myMethod("a", "b");
+    }
+}
