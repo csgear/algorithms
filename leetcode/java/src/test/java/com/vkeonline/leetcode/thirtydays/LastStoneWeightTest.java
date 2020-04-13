a5facf11ffcc2678d886dacf3f62f47b831fed3d
Xiaojun Yang <xyang126@ford.com>
2020-04-12 22:44:28 -0400

    last stone

--- leetcode/java/src/test/java/com/vkeonline/leetcode/thirtydays/LastStoneWeightTest.java
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/thirtydays/LastStoneWeightTest.java
@@ -1,6 +1,5 @@
 package com.vkeonline.leetcode.thirtydays;
 
-import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +8,8 @@ class LastStoneWeightTest {
     static LastStoneWeight lsw = new LastStoneWeight() ;
 
     @Test
-    @Description("expect returning 1")
     void lastStoneWeight() {
-        var stones = new int[] {2,7,4,1,8,1} ;
+        int[] stones = new int[] {2,7,4,1,8,1} ;
         int result = lsw.lastStoneWeight(stones) ;
         assertEquals(1, result);
     }
