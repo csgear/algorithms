2c11b917b908b7ec77a4c6845c04763362102abd
Xiaojun Yang <xyang126@ford.com>
2020-04-14 19:52:45 -0400

    assessment chapter 1

--- jdk11ocp/features/src/test/java/com/vkeonline/enthuware/PrimitiveDataTypeTest.java
+++ jdk11ocp/features/src/test/java/com/vkeonline/enthuware/PrimitiveDataTypeTest.java
@@ -10,4 +10,16 @@ class PrimitiveDataTypeTest {
     void myMethod() {
         pdt.myMethod("a", "b");
     }
+
+    @Test
+    void checkFloat() {
+        float f1 = 102 ;
+        float f2 = (int) 102.0 ;
+        // 0.0 default is double, won't compile
+        // float f3 = 1f * 0.0 ;
+        float f4 = 1f * (short) 0.0 ;
+        // 0 can not be converted to int, only string and boolean
+        // float f5 = 1f * (boolean) 0 ;
+
+    }
 }
