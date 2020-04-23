aeba0cbdd76d74f3169697d71824eb9bcbf63b2b
Xiaojun Yang <xyang126@ford.com>
2020-04-22 20:39:54 -0400

    comments for test 2

--- jdk11ocp/OCPFeatures/src/main/java/com/vkeonline/studyguide/AssessmentTwo.java
+++ jdk11ocp/OCPFeatures/src/main/java/com/vkeonline/studyguide/AssessmentTwo.java
@@ -1,4 +1,18 @@
 package com.vkeonline.studyguide;
 
+
+
+interface Account{
+    public default String getId(){
+        return "0000";
+    }
+}
+
+interface PremiumAccount extends Account{
+    String getId() ;
+}
+
+
+
 public class AssessmentTwo {
 }
