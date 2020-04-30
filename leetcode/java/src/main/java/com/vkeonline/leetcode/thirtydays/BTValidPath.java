aa41146ab1391c1b6ba2b51a6f1e5e14bee109d9
Xiaojun Yang <xyang126@ford.com>
2020-04-30 17:30:40 -0400

    day 30

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/thirtydays/BTValidPath.java
@@ -0,0 +1,23 @@
+package com.vkeonline.leetcode.thirtydays;
+
+import com.vkeonline.leetcode.common.TreeNode;
+
+public class BTValidPath {
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        return helper(root, arr, 0) ;
+    }
+
+    boolean helper(TreeNode node,int[] arr, int pos)
+    {
+        if(node== null)
+            return false;
+
+        if(node.val != arr[pos])
+            return false;
+
+        if(pos==arr.length-1)
+            return (node.left == null && node.right==null);
+
+        return(helper(node.left,arr,pos+1)||helper(node.right,arr,pos+1));
+    }
+}
