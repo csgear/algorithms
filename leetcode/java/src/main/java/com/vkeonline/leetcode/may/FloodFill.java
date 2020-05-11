bcc4320c283a21126225c49039259d2d1d4262a0
Xiaojun Yang <xyang126@ford.com>
2020-05-11 07:29:56 -0400

    may 11

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/may/FloodFill.java
@@ -0,0 +1,29 @@
+package com.vkeonline.leetcode.may;
+
+/**
+ * simple dfs
+ */
+public class FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+
+        int oldColor = image[sr][sc];
+
+        dfs(image, sr, sc, oldColor, newColor);
+
+        return image;
+    }
+
+    private void dfs(int[][] image, int i, int j, int oldColor, int newColor) {
+
+        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] == newColor)
+            return;
+
+        if (image[i][j] == oldColor) {
+            image[i][j] = newColor;
+            dfs(image, i + 1, j, oldColor, newColor);
+            dfs(image, i - 1, j, oldColor, newColor);
+            dfs(image, i, j + 1, oldColor, newColor);
+            dfs(image, i, j - 1, oldColor, newColor);
+        }
+    }
+}
