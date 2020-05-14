096a226fd909759fd99cfcb3917847d95b3a1f28
Xiaojun Yang <xyang126@ford.com>
2020-05-14 08:17:25 -0400

    may 14

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/may/Trie.java
@@ -0,0 +1,57 @@
+package com.vkeonline.leetcode.may;
+
+/**
+ * Implement Trie (Prefix Tree)
+ */
+public class Trie {
+    private class Node {
+        Node[] children = new Node[26]; // alphabet size
+        boolean isEnd = false;
+    }
+
+    Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+        root = new Node();
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        Node current = root;
+        for (char ch : word.toCharArray()) {
+            if (current.children[ch - 97] == null)
+                current.children[ch - 97] = new Node();
+            current = current.children[ch - 97];
+        }
+        current.isEnd = true;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        return isPrefixOrWord(word, false);
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        return isPrefixOrWord(prefix, true);
+    }
+
+    private boolean isPrefixOrWord(String word, boolean isPrefix) {
+        Node current = root;
+        for (char ch : word.toCharArray()) {
+            if (current.children[ch - 97] != null)
+                current = current.children[ch - 97];
+            else return false;
+        }
+        return (isPrefix) ? true : current.isEnd;
+    }
+}
