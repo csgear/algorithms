964f7accc7db70a6039883ac1db6bb51276195c2
Xiaojun Yang <xyang126@ford.com>
2020-05-16 07:12:51 -0400

    may 17

--- leetcode/java/src/main/java/com/vkeonline/leetcode/common/ListNode.java
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/common/ListNode.java
@@ -3,8 +3,17 @@ package com.vkeonline.leetcode.common;
 public class ListNode {
     public int val;
     public ListNode next;
+
     public ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    public ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
