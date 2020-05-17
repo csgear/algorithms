8e8f00a8a5f8797e6f6c856dda9a7a6c963c6291
Xiaojun Yang <xyang126@ford.com>
2020-05-17 10:40:17 -0400

    may 17

---
+++ lintcode/src/main/java/com/vkeonline/lintcode/RangeSumQuery2D.java
@@ -0,0 +1,28 @@
+package com.vkeonline.lintcode;
+
+/**
+ * LintCode: 665. Range Sum Query 2D - Immutable
+ */
+public class RangeSumQuery2D {
+}
+
+class NumMatrix {
+
+    private int[][] dp = null;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        dp = new int[m + 1][n + 1];
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                dp[row + 1][col + 1] = dp[row + 1][col] + dp[row][col + 1] - dp[row][col] + matrix[row][col];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
