eb6f12cebd580355694282a7943e34acb1444288
Xiaojun Yang <xyang126@ford.com>
2020-05-18 07:41:38 -0400

    may 18

--- leetcode/java/src/test/java/com/vkeonline/leetcode/may/MaximumSumCircularSubarrayTest.java
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/may/MaximumSumCircularSubarrayTest.java
@@ -2,22 +2,18 @@ package com.vkeonline.leetcode.may;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MaximumSumCircularSubarrayTest {
-    MaximumSumCircularSubarray maximumSumCircularSubarray = new MaximumSumCircularSubarray() ;
+    MaximumSumCircularSubarray maximumSumCircularSubarray = new MaximumSumCircularSubarray();
+
     @Test
     void maxSubarraySumCircular() {
-        int[] A = { 1,-2,3,-2} ;
-        assertEquals(3,maximumSumCircularSubarray.maxSubarraySumCircular(A));
-        int[] B = {5,-3,5} ;
+        int[] A = {1, -2, 3, -2};
+        assertEquals(3, maximumSumCircularSubarray.maxSubarraySumCircular(A));
+        int[] B = {5, -3, 5};
         assertEquals(10, maximumSumCircularSubarray.maxSubarraySumCircular(B));
     }
 
 
-    @Test
-    void maxSubarraySumCircular1() {
-        int[] B = {5,-3,5} ;
-        assertEquals(10, maximumSumCircularSubarray.maxSubarraySumCircular1(B));
-    }
 }
