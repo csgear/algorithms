70b65df7c180d210c64001cfce41c632346926ae
Xiaojun Yang <xyang126@ford.com>
2020-05-28 08:04:31 -0400

    some solutions from may27

---
+++ lintcode/src/test/java/com/vkeonline/lintcode/TopKthMentionedTest.java
@@ -0,0 +1,16 @@
+package com.vkeonline.lintcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopKthMentionedTest {
+
+    @Test
+    void topkKeywords() {
+        String s = "Best services provided by   anacell, everyone should use anacell" ;
+        s.replaceAll("", "") ;
+        String[] token = s.split("\\s+") ;
+        System.out.println(token.length);
+    }
+}
