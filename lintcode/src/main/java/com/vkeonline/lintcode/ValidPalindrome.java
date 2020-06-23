0362d905975b4126026441f9562719e4a597ccb0
Xiaojun Yang <xyang126@ford.com>
2020-06-23 06:18:59 -0400

    facebook

---
+++ lintcode/src/main/java/com/vkeonline/lintcode/ValidPalindrome.java
@@ -0,0 +1,44 @@
+package com.vkeonline.lintcode;
+
+/**
+ * @author csgear
+ * Lintcode: 415. Valid Palindrome
+ */
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0) {
+            return true ;
+        }
+
+        char[] chars = s.toCharArray() ;
+        int i = 0, j = chars.length - 1 ;
+        boolean result = true ;
+
+        while(i < j) {
+            while( i < s.length() && !isValid(chars[i])) {
+                i++ ;
+            }
+
+            if(i == chars.length) {
+                break ;
+            }
+
+            while(j >= 0 && !isValid(chars[j])) {
+                j-- ;
+            }
+
+            if(Character.toLowerCase(chars[i])  != Character.toLowerCase(chars[j])) {
+                result = false ;
+                break ;
+            }
+
+            i++ ; j-- ;
+        }
+
+        return result ;
+    }
+
+    private boolean isValid(char c) {
+        return Character.isLetter(c) || Character.isDigit(c) ;
+    }
+}
