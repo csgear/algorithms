4af93b4c9d2358e0db74d3fc3284a8606138721a
Xiaojun Yang <xyang126@ford.com>
2020-06-24 10:12:16 -0400

    june 24

--- leetcode/java/src/main/java/com/vkeonline/leetcode/june/UniqueBinarySearchTrees.java
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/june/UniqueBinarySearchTrees.java
@@ -5,11 +5,13 @@ package com.vkeonline.leetcode.june;
  */
 public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
-        if(n == 0 || n == 1)
+        if (n == 0 || n == 1) {
             return 1;
+        }
         int count = 0;
-        for(int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++) {
             count += numTrees(i - 1) * numTrees(n - i);
+        }
         return count;
 
     }
