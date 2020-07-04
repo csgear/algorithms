f6d70161efa7d2c81393da8b2e633877500ccf6e
Xiaojun Yang <xyang126@ford.com>
2020-07-04 08:21:04 -0400

    July 4

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/july/UglyNumberII.java
@@ -0,0 +1,33 @@
+package com.vkeonline.leetcode.july;
+
+/**
+ * @author csgear
+ */
+public class UglyNumberII {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[1690];
+        dp[0] = 1;
+        int countTwo = 0, countThree = 0, countFive = 0;
+
+        for (int i = 1; i < 1690; i++) {
+            int currTwo = dp[countTwo] * 2;
+            int currThree = dp[countThree] * 3;
+            int currFive = dp[countFive] * 5;
+
+            dp[i] = Math.min (currTwo, Math.min (currThree, currFive));
+
+            if (dp[i] == currTwo) {
+                ++countTwo;
+            }
+            if (dp[i] == currThree) {
+                ++countThree;
+            }
+            if (dp[i] == currFive) {
+                ++countFive;
+            }
+        }
+
+        return dp[n-1] ;
+
+    }
+}
