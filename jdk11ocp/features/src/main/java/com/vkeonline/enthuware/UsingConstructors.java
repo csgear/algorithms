7fcdf64c4e95e147f2e312bf18c4f84b4c43da55
Xiaojun Yang <xyang126@ford.com>
2020-07-09 07:21:29 -0400

    july 9

--- jdk11ocp/features/src/main/java/com/vkeonline/enthuware/UsingConstructors.java
+++ jdk11ocp/features/src/main/java/com/vkeonline/enthuware/UsingConstructors.java
@@ -1,11 +1,6 @@
 package com.vkeonline.enthuware;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 class Game {
     static int x = 5;
 
@@ -30,6 +25,8 @@ class Game {
 }
 
 class Soccer extends Game {
+
+
     @Override
     public void play() {
         System.out.println("Playing Soccer...");
@@ -37,24 +34,104 @@ class Soccer extends Game {
 
 
     public static void main(String[] args) throws Exception {
-        System.out.println(args.length);
-        System.out.println(args == null);
-        System.out.println(1 + 5 < 3 + 7);
-        System.out.println((2 + 2) >= 2 + 3);
 
-        var values = new ArrayList<String>();
-        values.add("hello");
-        values.add("world");
-        values.add("peter");
-        values.sort((a, b) -> a.compareTo(b));
-        String[] sa = values.toArray(new String[]{});
-        System.out.println(Arrays.toString(sa));
-        values.forEach(System.out::println);
-        values.sort((var a, var b) -> a.compareTo(b));
-        values.removeIf((var k) -> k.equals("a"));
+        int index = 1;
+        String[] strArr = new String[5];
+        String myStr = strArr[index];
+        System.out.println(myStr);
+
     }
 }
 
 
+class ParamTest {
+    public static void printSum(int a, int b) {
+        System.out.println("In int " + (a + b));
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println("In Integer " + (a + b));
+    }
+
+    public static void printSum(double a, double b) {
+        System.out.println("In double " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        Integer val1 = Integer.valueOf(5) ;
+        int val2 = 9;
+        testInts(val1++, ++val2);
+        System.out.println(val1 + " " + val2);
+    }
+
+    public static void testInts(Integer obj, int var) {
+        obj = var++;
+        obj++;
+    }
+}
 
 
+interface ConstTest {
+    public int A = 1;
+    int B = 1;
+    static int C = 1;
+    final int D = 1;
+    public static int E = 1;
+    public final int F = 1;
+    static final int G = 1;
+    public static final int H = 1;
+}
+
+interface Device {
+    public abstract void switchOn();
+}
+
+abstract class Router /* LOCATION 1 */ {    /* LOCATION 2 */
+    public void switchOn() {
+    }
+
+    public abstract void reset();
+}
+
+class MimoRouter extends Router implements Device {
+
+    @Override
+    public void reset() {
+    }
+}
+
+class TestClass {
+    public static void main(String[] args) {
+        try {
+            doTest();
+        } catch (MyException me) {
+            System.out.println(me);
+        }
+    }
+
+    static void doTest() throws MyException {
+        int[] array = new int[10];
+        array[10] = 1000;
+        doAnotherTest();
+    }
+
+    static void doAnotherTest() throws MyException {
+        throw new MyException("Exception from doAnotherTest");
+    }
+}
+
+class MyException extends Exception {
+    public MyException(String msg) {
+        super(msg);
+    }
+
+}
+
+interface Account {
+    public default String getId() {
+        return "0000";
+    }
+}
+
+interface PremiumAccount extends Account {
+}
