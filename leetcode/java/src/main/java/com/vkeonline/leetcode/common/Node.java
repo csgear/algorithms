bbc5550c888fd504ab7f6f18cca63d74c71078e8
Xiaojun Yang <xyang126@ford.com>
2020-07-10 13:47:52 -0400

    July 10

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/common/Node.java
@@ -0,0 +1,12 @@
+package com.vkeonline.leetcode.common;
+
+/**
+ * @author csgear
+ */
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+
