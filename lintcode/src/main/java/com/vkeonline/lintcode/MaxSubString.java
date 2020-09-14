e3ee1e116983dc7e918250045638d5a174a12553
Xiaojun Yang <xyang126@ford.com>
2020-09-14 08:57:58 -0400

    sept 14

---
+++ lintcode/src/main/java/com/vkeonline/lintcode/MaxSubString.java
@@ -0,0 +1,47 @@
+package com.vkeonline.lintcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Lintcode: 1648. max substring
+ * @author csgear
+ */
+public class MaxSubString {
+    public String maxSubstring(String s) {
+        List<Integer> pos = new ArrayList<>() ;
+
+        char maxChar = 'a' ;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) > maxChar) {
+                pos.clear();
+                pos.add(i) ;
+                maxChar = s.charAt(i) ;
+            }
+            else if(s.charAt(i) == maxChar){
+                pos.add(i) ;
+            }
+        }
+
+        if(pos.size() == 1)  {
+            return s.substring(pos.get(0)) ;
+        }
+
+        int prePos = pos.get(0) ;
+        for(int i = 1 ; i < pos.size() ; i++) {
+            int currPos = pos.get(i) ;
+            for (int j = 0; j < s.length() - currPos; j++) {
+                if(s.charAt(currPos + j) > s.charAt(prePos + j)) {
+                    prePos = currPos ;
+                    break ;
+                }
+                if(s.charAt(currPos + j) < s.charAt(prePos + j)) {
+                    break ;
+                }
+            }
+        }
+
+        return s.substring(prePos) ;
+    }
+
+}
---
+++ lintcode/src/main/java/com/vkeonline/lintcode/RestoreIPAddresses.java
@@ -0,0 +1,15 @@
+package com.vkeonline.lintcode;
+
+import java.util.List;
+
+/**
+ * Lintcode: 426. Restore IP Addresses
+ * @author csgear
+ */
+public class RestoreIPAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        return null ;
+    }
+
+
+}
