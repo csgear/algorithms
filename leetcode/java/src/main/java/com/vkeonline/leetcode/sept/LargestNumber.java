2b91319b0347d11bc731d46e09e227c65f90cc9b
Xiaojun Yang <xyang126@ford.com>
2020-09-25 16:10:33 -0400

    Sept 25

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/sept/LargestNumber.java
@@ -0,0 +1,18 @@
+package com.vkeonline.leetcode.sept;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @author csgear
+ */
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+
+        String result = Arrays.stream(nums).
+                mapToObj(String::valueOf).sorted((a, b) -> (b + a).
+                compareTo(a + b)).collect(Collectors.joining());
+
+        return result.startsWith("0") ? "0" : result;
+    }
+}
