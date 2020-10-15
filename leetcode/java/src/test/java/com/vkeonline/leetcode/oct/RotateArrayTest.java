06a6457ada57a5e54701702704a3a430632b47c6
Xiaojun Yang <xyang126@ford.com>
2020-10-15 08:35:11 -0400

    oCT 15

---
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/oct/RotateArrayTest.java
@@ -0,0 +1,21 @@
+package com.vkeonline.leetcode.oct;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RotateArrayTest {
+
+    @Test
+    void rotate() {
+        int[] nums = {1,2,3,4,5,6,7} ;
+        int k = 3 ;
+        int[] expected = {5,6,7,1,2,3,4} ;
+
+        RotateArray rotateArray = new RotateArray() ;
+
+        rotateArray.rotate(nums, k);
+
+        assertArrayEquals(expected, nums);
+    }
+}
