f8eb16ac294c0560b6b291510953d4a8ae64423f
Xiaojun Yang <xyang126@ford.com>
2020-11-18 08:51:30 -0500

    - nov 18 problem

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/year2020/nov/MergeIntervals.java
@@ -0,0 +1,33 @@
+package com.vkeonline.leetcode.year2020.nov;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author csgear
+ */
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0])) ;
+
+        int start = intervals[0][0] ;
+        int end = intervals[0][1] ;
+
+        for(int[] interval: intervals) {
+            if(interval[0] <= end) {
+                end = Math.max(end, interval[1]) ;
+            }
+            else {
+                res.add(new int[] {start, end}) ;
+                start = interval[0] ;
+                end = interval[1] ;
+            }
+        }
+
+        res.add(new int[] {start, end}) ;
+        return res.toArray(new int[0][]) ;
+    }
+}
---
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/year2020/nov/MergeIntervalsTest.java
@@ -0,0 +1,24 @@
+package com.vkeonline.leetcode.year2020.nov;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeIntervalsTest {
+
+    @Test
+    void testMerge() {
+        MergeIntervals mergeIntervals = new MergeIntervals();
+
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+
+        int[][] expected = {{1,6},{8,10},{15,18}} ;
+
+        int[][] result = mergeIntervals.merge(intervals) ;
+
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], result[i]);
+        }
+
+    }
+
+}
