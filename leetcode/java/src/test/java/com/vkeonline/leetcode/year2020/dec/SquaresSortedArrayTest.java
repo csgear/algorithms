1ee13f53074bc4fcbfc713f5b4ec61ba923ee72f
Xiaojun Yang <xyang126@ford.com>
2020-12-15 09:01:34 -0500

    - December 15, 2020

---
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/year2020/dec/SquaresSortedArrayTest.java
@@ -0,0 +1,18 @@
+package com.vkeonline.leetcode.year2020.dec;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquaresSortedArrayTest {
+
+    @Test
+    void sortedSquares() {
+        int[] nums = {-4,-1,0,3,10} ;
+        int[] expected = {0,1,9,16,100} ;
+
+        SquaresSortedArray squaresSortedArray = new SquaresSortedArray() ;
+
+        assertArrayEquals(expected, squaresSortedArray.sortedSquares(nums));
+    }
+}
