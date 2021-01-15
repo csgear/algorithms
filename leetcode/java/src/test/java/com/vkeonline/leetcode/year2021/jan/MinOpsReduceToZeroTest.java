fc71d23010951ba76b354a24d48241f01012b0aa
Xiaojun Yang <xyang126@ford.com>
2021-01-15 08:25:18 -0500

    Jan 14

---
+++ leetcode/java/src/test/java/com/vkeonline/leetcode/year2021/jan/MinOpsReduceToZeroTest.java
@@ -0,0 +1,17 @@
+package com.vkeonline.leetcode.year2021.jan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinOpsReduceToZeroTest {
+    MinOpsReduceToZero minOpsReduceToZero = new MinOpsReduceToZero() ;
+
+    @Test
+    void minOperations() {
+        int[] nums = {1,1,4,2,3} ;
+        int x = 5 ;
+        int expected = 2 ;
+        assertEquals(expected, minOpsReduceToZero.minOperations(nums, x));
+    }
+}
