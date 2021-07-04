138ad8d0485868268913678455873ec55b60f0ee
Xiaojun Yang <csgear@live.com>
2021-07-04 11:39:37 -0400

    July 2021: Leetcode Code Challenge
    - July 4 problem sets

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/p1200/CountVowelsPermutation.java
@@ -0,0 +1,28 @@
+package com.vkeonline.leetcode.p1200;
+
+/**
+ * Leetcode [H]: 1220. Count Vowels Permutation
+ *
+ * @author csgear
+ */
+public class CountVowelsPermutation {
+    public int countVowelPermutation(int n) {
+        long aCount = 1, eCount = 1, iCount = 1, oCount = 1, uCount = 1;
+        int MOD = 1000000007;
+
+        for (int i = 1; i < n; i++) {
+            long aCountNew = (eCount + iCount + uCount) % MOD;
+            long eCountNew = (aCount + iCount) % MOD;
+            long iCountNew = (eCount + oCount) % MOD;
+            long oCountNew = (iCount) % MOD;
+            long uCountNew = (iCount + oCount) % MOD;
+            aCount = aCountNew;
+            eCount = eCountNew;
+            iCount = iCountNew;
+            oCount = oCountNew;
+            uCount = uCountNew;
+        }
+        long result = (aCount + eCount + iCount + oCount + uCount) % MOD;
+        return (int) result;
+    }
+}
