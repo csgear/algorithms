bbc5854122179ac198f17c9ee43632f6b33b2182
xiaoyang <xiaojun.yang.ext@nokia.com>
2021-10-02 09:21:33 -0400

    Dynamic Programming Day 4

---
+++ leetcode/java/src/main/java/com/vkeonline/leetcode/p200/PaintHouse.java
@@ -0,0 +1,13 @@
+
+package com.vkeonline.leetcode.p200;
+
+/**
+ * Leetcode [M]: 256. Paint House
+ * @author csgear
+ */
+public class PaintHouse {
+    public int minCost(int[][] costs) {
+        return 0 ;
+    }
+
+}
